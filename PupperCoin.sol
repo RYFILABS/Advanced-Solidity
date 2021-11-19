e324d414049fdd0c30c793b0f099769e899deca1
Ryan Weier <74256787+weiermedia@users.noreply.github.com>
2021-11-19 11:18:03 -0500

    Update PupperCoin.sol

--- PupperCoin.sol
+++ PupperCoin.sol
@@ -1,8 +1,9 @@
+
 pragma solidity ^0.5.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Mintable.sol";
 
 contract PupperCoin is ERC20, ERC20Detailed, ERC20Mintable {
     constructor(
@@ -13,6 +14,7 @@ contract PupperCoin is ERC20, ERC20Detailed, ERC20Mintable {
         ERC20Detailed(name, symbol, 18)
         public
     {
-        // constructor can stay empty
-    }
+       
+    }    
+        
 }
